a3b7b17bc9399741789d910bc3b3694daea58fde
a0935210570602 <a0935210570602@gmail.com>
2025-04-04 15:39:31 +0800

    Add __pycache__ to .gitignore

--- run_experiments.py
+++ run_experiments.py
@@ -5,15 +5,15 @@ import matplotlib
 matplotlib.rcParams['font.family'] = 'DejaVu Sans'  # 或 'sans-serif'
 matplotlib.rcParams['text.usetex'] = False
 
-normal_filename = 'raw_data/age_below_65_health.csv'
-stroke_filename = 'raw_data/age_below_65_stroke.csv'
-model_prediction = SecondStrokePrediction(normal_filename, stroke_filename)
-model_prediction.predict_svm_linear()
-model_prediction.predict_svm_poly()
-model_prediction.predict_svm_rbf()
-model_prediction.predict_decision_tree()
-model_prediction.predict_random_forest()
-model_prediction.predict_adaboost()
-model_prediction.predict_gradient_boost()
-model_prediction.predict_xgboost()
-model_prediction.show_all_result()
+normal_filename = 'data/age_below_65_health.csv'
+stroke_filename = 'data/age_below_65_stroke.csv'
+# model_prediction = SecondStrokePrediction(normal_filename, stroke_filename)
+# model_prediction.predict_svm_linear()
+# model_prediction.predict_svm_poly()
+# model_prediction.predict_svm_rbf()
+# model_prediction.predict_decision_tree()
+# model_prediction.predict_random_forest()
+# model_prediction.predict_adaboost()
+# model_prediction.predict_gradient_boost()
+# model_prediction.predict_xgboost()
+# model_prediction.show_all_result()
