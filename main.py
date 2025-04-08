384a9b3dd14838ed7712f39b12d75c4ed780e51a
a0935210570602 <a0935210570602@gmail.com>
2025-04-08 17:47:06 +0800

    Feature:Add run all exp function

--- main.py
+++ main.py
@@ -1,172 +1,27 @@
 from second_recurrent_prediction import SecondStrokePrediction
 from sklearn.exceptions import DataConversionWarning, ConvergenceWarning
 from sklearn.exceptions import UndefinedMetricWarning
-import warnings
+from exp_config import smote_methods, feature_selection_methods, prediction_methods
 
+import warnings
 warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
 warnings.filterwarnings("ignore", category=DataConversionWarning)
-warnings.filterwarnings("ignore", message="The least populated class in y has only")
 warnings.filterwarnings("ignore", category=ConvergenceWarning)
+warnings.filterwarnings("ignore", message="The least populated class in y has only")
+
 
 file_path = 'raw_data/age_below_65_91_samples.csv'
 model_prediction = SecondStrokePrediction(file_path)
 
-model_prediction.load_data()
-model_prediction.standardize_data()
-model_prediction.smote_borderline()
-model_prediction.sfs_logistic_feature_selection()
-
-model_prediction.predict_svm_linear()
-model_prediction.predict_svm_poly()
-model_prediction.predict_svm_rbf()
-model_prediction.predict_decision_tree()
-model_prediction.predict_random_forest()
-model_prediction.predict_adaboost()
-model_prediction.predict_gradient_boost()
-model_prediction.predict_xgboost()
-
-############################################
-
-model_prediction.load_data()
-model_prediction.standardize_data()
-model_prediction.smote_borderline()
-model_prediction.sfs_knn_feature_selection()
- 
-model_prediction.predict_svm_linear()
-model_prediction.predict_svm_poly()
-model_prediction.predict_svm_rbf()
-model_prediction.predict_decision_tree()
-model_prediction.predict_random_forest()
-model_prediction.predict_adaboost()
-model_prediction.predict_gradient_boost()
-model_prediction.predict_xgboost()
-
-############################################
-
-model_prediction.load_data()
-model_prediction.standardize_data()
-model_prediction.smote_borderline()
-model_prediction.sfs_linear_feature_selection()
-
-model_prediction.predict_svm_linear()
-model_prediction.predict_svm_poly()
-model_prediction.predict_svm_rbf()
-model_prediction.predict_decision_tree()
-model_prediction.predict_random_forest()
-model_prediction.predict_adaboost()
-model_prediction.predict_gradient_boost()
-model_prediction.predict_xgboost()
-
-############################################
-
-model_prediction.load_data()
-model_prediction.standardize_data()
-model_prediction.smote_borderline()
-model_prediction.sfs_random_forest_feature_selection()
-
-model_prediction.predict_svm_linear()
-model_prediction.predict_svm_poly()
-model_prediction.predict_svm_rbf()
-model_prediction.predict_decision_tree()
-model_prediction.predict_random_forest()
-model_prediction.predict_adaboost()
-model_prediction.predict_gradient_boost()
-model_prediction.predict_xgboost()
-
-############################################
-
-model_prediction.load_data()
-model_prediction.standardize_data()
-model_prediction.smote_borderline()
-model_prediction.sfs_xgboost_feature_selection()
-
-model_prediction.predict_svm_linear()
-model_prediction.predict_svm_poly()
-model_prediction.predict_svm_rbf()
-model_prediction.predict_decision_tree()
-model_prediction.predict_random_forest()
-model_prediction.predict_adaboost()
-model_prediction.predict_gradient_boost()
-model_prediction.predict_xgboost()
-
-############################################
-
-model_prediction.load_data()
-model_prediction.standardize_data()
-model_prediction.smote_borderline()
-model_prediction.permutation_feature_selection()
-
-model_prediction.predict_svm_linear()
-model_prediction.predict_svm_poly()
-model_prediction.predict_svm_rbf()
-model_prediction.predict_decision_tree()
-model_prediction.predict_random_forest()
-model_prediction.predict_adaboost()
-model_prediction.predict_gradient_boost()
-model_prediction.predict_xgboost()
-
-############################################
-
-model_prediction.load_data()
-model_prediction.standardize_data()
-model_prediction.smote_borderline()
-model_prediction.boruta_feature_selection()
-
-model_prediction.predict_svm_linear()
-model_prediction.predict_svm_poly()
-model_prediction.predict_svm_rbf()
-model_prediction.predict_decision_tree()
-model_prediction.predict_random_forest()
-model_prediction.predict_adaboost()
-model_prediction.predict_gradient_boost()
-model_prediction.predict_xgboost()
-
-############################################
-
-model_prediction.load_data()
-model_prediction.standardize_data()
-model_prediction.smote_borderline()
-model_prediction.rfecv_feature_selection()
-
-model_prediction.predict_svm_linear()
-model_prediction.predict_svm_poly()
-model_prediction.predict_svm_rbf()
-model_prediction.predict_decision_tree()
-model_prediction.predict_random_forest()
-model_prediction.predict_adaboost()
-model_prediction.predict_gradient_boost()
-model_prediction.predict_xgboost()
-
-############################################
-
-model_prediction.load_data()
-model_prediction.standardize_data()
-model_prediction.smote_borderline()
-model_prediction.variance_threshold_selection()
-
-model_prediction.predict_svm_linear()
-model_prediction.predict_svm_poly()
-model_prediction.predict_svm_rbf()
-model_prediction.predict_decision_tree()
-model_prediction.predict_random_forest()
-model_prediction.predict_adaboost()
-model_prediction.predict_gradient_boost()
-model_prediction.predict_xgboost()
-
-############################################
-
-model_prediction.load_data()
-model_prediction.standardize_data()
-model_prediction.smote_borderline()
-model_prediction.l1_feature_selection()
-
-model_prediction.predict_svm_linear()
-model_prediction.predict_svm_poly()
-model_prediction.predict_svm_rbf()
-model_prediction.predict_decision_tree()
-model_prediction.predict_random_forest()
-model_prediction.predict_adaboost()
-model_prediction.predict_gradient_boost()
-model_prediction.predict_xgboost()
-
-############################################
+# 對於每一種 SMOTE 與特徵選擇的組合
+for data_smote in smote_methods:
+    for data_feature_selection in feature_selection_methods:
+        for model_predict in prediction_methods:
+            # 每次實驗從原始資料開始：重新載入與標準化
+            file_path = 'raw_data/age_below_65_91_samples.csv'
+            model_prediction = SecondStrokePrediction(file_path)
+            model_prediction.load_data()
+            model_prediction.standardize_data()
+            data_smote(model_prediction)
+            data_feature_selection(model_prediction)
+            model_predict(model_prediction)
